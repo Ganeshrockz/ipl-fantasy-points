16a90f00394ebde819f5be6d91b710050ff9b72a
Ganeshrockz <ganes@microsoft.com>
2020-10-28 23:07:38 +0530

    Updates

--- precache-manifest.f9535cec43f389c7d11cff0f252a43cc.js
+++ precache-manifest.f9535cec43f389c7d11cff0f252a43cc.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "6eb0371367eddffc24d3",
+    "revision": "b0f64dfdff49066a4c51",
     "url": "/ipl-fantasy-points/static/css/main.b9b24dce.chunk.css"
   },
   {
-    "revision": "6eb0371367eddffc24d3",
-    "url": "/ipl-fantasy-points/static/js/main.6eb03713.chunk.js"
+    "revision": "b0f64dfdff49066a4c51",
+    "url": "/ipl-fantasy-points/static/js/main.b0f64dfd.chunk.js"
   },
   {
     "revision": "7a120c796ff37bd32dd2",
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/ipl-fantasy-points/static/js/2.884f9fd5.chunk.js"
   },
   {
-    "revision": "13a7d29bb13127a5e6e26f39477cfc49",
+    "revision": "da57128aa0b491e06e02dc4fb5c94535",
     "url": "/ipl-fantasy-points/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ipl-fantasy-points/precache-manifest.19099274f6799c530714ec9ffd3654cd.js"
+  "/ipl-fantasy-points/precache-manifest.f9535cec43f389c7d11cff0f252a43cc.js"
 );
 
 workbox.clientsClaim();
